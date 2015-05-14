3646474602e6c72b5671e1ebcaa45799a9cb176d
Fernando Daciuk <f.daciuk@gmail.com>
2015-05-14 00:08:17 -0300

    Add plato

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,23 @@
 var gulp = require( 'gulp' );
+var plato = require( 'plato' );
 var exec = require( 'child_process' ).exec;
+var moduleDir = '../../ajax';
 
-gulp.task( 'deploy', function( done ) {
-    return exec( 'cp -R ../../ajax/coverage ../../ajax/plato ./', function( err, stdout, stderr ) {
+gulp.task( 'deploy', [ 'plato' ], function( done ) {
+    return exec( 'cp -R ' + moduleDir + '/coverage ' + moduleDir + '/plato ./',
+    function( err, stdout, stderr ) {
         exec( 'git push origin gh-pages', function() {
             done();
         });
     });
+});
+
+gulp.task( 'plato', function( done ) {
+  var files = [ moduleDir + '/src/**/*.js' ];
+  var outputDir = './plato';
+  var options = { title: '#Ajax' };
+  function callback( report ) { done(); };
+  plato.inspect( files, outputDir, options, callback );
 });
 
 gulp.task( 'default', [ 'deploy' ] );
