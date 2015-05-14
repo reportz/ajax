3fb7c40076a3f5f505e9ff4fd15245a49d3f2f39
Fernando Daciuk <f.daciuk@gmail.com>
2015-05-13 23:48:34 -0300

    Add task default

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,10 @@ var exec = require( 'child_process' ).exec;
 
 gulp.task( 'deploy', function( done ) {
     return exec( 'cp -R ../../ajax/coverage ../../ajax/plato ./', function( err, stdout, stderr ) {
-        exec( 'echo "Copied!"', function() {
+        exec( 'git push origin gh-pages', function() {
             done();
         });
     });
 });
+
+gulp.task( 'default', [ 'deploy' ] );
